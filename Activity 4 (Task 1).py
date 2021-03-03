4baa96dbae8ae26a64fe30244e6f1e5f2b4b3792
ChristianCasino <78083258+ChristianCasino@users.noreply.github.com>
2021-03-03 14:38:40 +0800

    Add files via upload

---
+++ Activity 4 (Task 1).py
@@ -0,0 +1,98 @@
+Vector1 = [(1,2,-3,4)]
+a = []
+
+for numbers in Vector1:
+    for number in numbers:
+        square1 = number **2
+        a.append(square1)
+
+print('Given:                       ',Vector1)
+print('The square of all numbers    ',a)
+print('The sum of all numbers:      ',sum(a))
+print('The square root of the sum:  ',sum(a)**(1/2))
+print('#'*64)
+#####################################################
+Vector2 = [(1,2,-3,2)]
+b = []
+
+for numbers in Vector2:
+    for number in numbers:
+        square2 = number **2
+        b.append(square2)
+
+print('Given:                       ',Vector2)
+print('The square of all numbers    ',b)
+print('The sum of all numbers:      ',sum(b))
+print('The square root of the sum:  ',sum(b)**(1/2))
+print('#'*64)
+#####################################################
+Vector3 = [(2,2,2,2)]
+c = []
+
+for numbers in Vector3:
+    for number in numbers:
+        square3 = number **2
+        c.append(square3)
+
+print('Given:                       ',Vector3)
+print('The square of all numbers    ',c)
+print('The sum of all numbers:      ',sum(c))
+print('The square root of the sum:  ',sum(c)**(1/2))
+print('#'*64)
+#####################################################
+Vector4 = [(3,3,3,3)]
+d = []
+
+for numbers in Vector4:
+    for number in numbers:
+        square4 = number **2
+        d.append(square4)
+
+print('Given:                       ',Vector4)
+print('The square of all numbers    ',d)
+print('The sum of all numbers:      ',sum(d))
+print('The square root of the sum:  ',sum(d)**(1/2))
+print('#'*64)
+#####################################################
+Vector5 = [(4,4,4,4)]
+e = []
+
+for numbers in Vector5:
+    for number in numbers:
+        square5 = number **2
+        e.append(square5)
+
+print('Given:                       ',Vector5)
+print('The square of all numbers    ',e)
+print('The sum of all numbers:      ',sum(e))
+print('The square root of the sum:  ',sum(e)**(1/2))
+print('#'*64)
+
+
+#####################################################
+Vector6 = [(5,5,5,5)]
+f = []
+
+for numbers in Vector6:
+    for number in numbers:
+        square6 = number **2
+        f.append(square6)
+
+print('Given:                       ',Vector6)
+print('The square of all numbers    ',f)
+print('The sum of all numbers:      ',sum(f))
+print('The square root of the sum:  ',sum(f)**(1/2))
+print('#'*64)
+
+
+
+
+
+
+
+
+
+
+
+
+
